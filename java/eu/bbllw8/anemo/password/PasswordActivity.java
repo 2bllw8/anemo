c30a98d15120273bf9e8142462f6bb97917444bf
2bllw8 <2bllw8@gmail.com>
2021-05-13 11:59:14 +0200

    Add password module

---
+++ java/eu/bbllw8/anemo/password/PasswordActivity.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021 2bllw8
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+package eu.bbllw8.anemo.password;
+
+import android.app.Activity;
+
+public final class PasswordActivity extends Activity {
+
+}
