65d1acc8ce0bfd484034e7b1d4221d190ca5e183
2bllw8 <2bllw8@gmail.com>
2021-06-03 12:46:00 +0200

    Add auto lock after 15 minutes

--- java/eu/bbllw8/anemo/lock/LockStore.java
+++ java/eu/bbllw8/anemo/lock/LockStore.java
@@ -4,6 +4,9 @@
  */
 package eu.bbllw8.anemo.lock;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
@@ -20,10 +23,15 @@ import java.util.function.Consumer;
 
 public final class LockStore implements SharedPreferences.OnSharedPreferenceChangeListener {
     private static final String TAG = "LockStore";
+
     private static final String LOCK_PREFERENCES = "lock_store";
     private static final String KEY_LOCK = "is_locked";
     private static final String KEY_PASSWORD = "password_hash";
     private static final boolean DEFAULT_LOCK_VALUE = false;
+
+    private static final int AUTO_LOCK_JOB_ID = 64;
+    // 15 minutes in milliseconds
+    private static final long AUTO_LOCK_DELAY = 1000L * 60L * 15L;
 
     @NonNull
     private final SharedPreferences preferences;
@@ -32,8 +40,14 @@ public final class LockStore implements SharedPreferences.OnSharedPreferenceChan
     @NonNull
     private final SparseArray<Consumer<Boolean>> listeners;
 
+    @NonNull
+    private final JobScheduler jobScheduler;
+    @NonNull
+    private final ComponentName autoLockComponent;
+
     private static volatile LockStore instance;
 
+    @NonNull
     public static LockStore getInstance(@NonNull Context context) {
         if (instance == null) {
             synchronized (LockStore.class) {
@@ -51,11 +65,15 @@ public final class LockStore implements SharedPreferences.OnSharedPreferenceChan
 
         listeners = new SparseArray<>();
         isLocked = new AtomicBoolean(preferences.getBoolean(KEY_LOCK, DEFAULT_LOCK_VALUE));
+
+        jobScheduler = context.getSystemService(JobScheduler.class);
+        autoLockComponent = new ComponentName(context, AutoLockJobService.class);
     }
 
     @Override
     protected void finalize() throws Throwable {
         preferences.unregisterOnSharedPreferenceChangeListener(this);
+        jobScheduler.cancel(AUTO_LOCK_JOB_ID);
         super.finalize();
     }
 
@@ -77,12 +95,18 @@ public final class LockStore implements SharedPreferences.OnSharedPreferenceChan
     }
 
     public synchronized void lock() {
+        jobScheduler.cancel(AUTO_LOCK_JOB_ID);
         preferences.edit()
                 .putBoolean(KEY_LOCK, true)
                 .apply();
     }
 
     public synchronized void unlock() {
+        final JobInfo jobInfo = new JobInfo.Builder(AUTO_LOCK_JOB_ID, autoLockComponent)
+                .setMinimumLatency(AUTO_LOCK_DELAY)
+                .build();
+        jobScheduler.schedule(jobInfo);
+
         preferences.edit()
                 .putBoolean(KEY_LOCK, false)
                 .apply();
