ca4205f1cc06036784743e3734e1ac299b35aed1
2bllw8 <2bllw8@gmail.com>
2022-03-15 21:44:23 +0100

    Make the receiver a dialog instead of a notification
    Because it's not possible to hide the app icon if foreground services are used...
    For detailed explaination and reproduce code see: https://github.com/2bllw8/android-reproduce-synthetic-activity

--- app/src/main/java/exe/bbllw8/anemo/documents/receiver/ReceiverActivity.java
+++ app/src/main/java/exe/bbllw8/anemo/documents/receiver/ReceiverActivity.java
@@ -5,26 +5,130 @@
 package exe.bbllw8.anemo.documents.receiver;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+
+import exe.bbllw8.anemo.R;
+import exe.bbllw8.anemo.documents.home.HomeEnvironment;
+import exe.bbllw8.anemo.task.TaskExecutor;
+
 public final class ReceiverActivity extends Activity {
+    private static final String TAG = "ReceiverActivity";
+
+    private final TaskExecutor taskExecutor = new TaskExecutor();
+    private final AtomicReference<Optional<Dialog>> dialogRef
+            = new AtomicReference<>(Optional.empty());
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         final Intent intent = getIntent();
-        if (intent != null && Intent.ACTION_SEND.equals(intent.getAction())) {
-            final Intent serviceIntent = new Intent(this, ReceiverService.class)
-                    .setType(intent.getType())
-                    .putExtra(Intent.EXTRA_STREAM,
-                            (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM));
-            startForegroundService(serviceIntent);
+        if (intent == null || !Intent.ACTION_SEND.equals(intent.getAction())) {
+            Log.e(TAG, "Nothing to do");
+            finish();
+            return;
         }
-        finish();
+
+        final String type = intent.getType();
+        if (type != null) {
+            try {
+                final Importer[] importers = getImporters();
+                for (final Importer importer : importers) {
+                    if (importer.typeMatch(type)) {
+                        runImporter(importer, intent);
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "Failed import", e);
+                finish();
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        dialogRef.get().ifPresent(Dialog::dismiss);
+        taskExecutor.terminate();
+        super.onDestroy();
+    }
+
+    private Importer[] getImporters() throws IOException {
+        final HomeEnvironment homeEnvironment = HomeEnvironment.getInstance(this);
+        final Path fallbackDir = homeEnvironment.getBaseDir();
+        return new Importer[]{
+                // Audio
+                new Importer(this,
+                        taskExecutor,
+                        homeEnvironment.getDefaultDirectory(HomeEnvironment.MUSIC)
+                                .orElse(fallbackDir),
+                        "audio/",
+                        R.string.receiver_audio_default_name),
+                // Images
+                new Importer(this,
+                        taskExecutor,
+                        homeEnvironment.getDefaultDirectory(HomeEnvironment.PICTURES)
+                                .orElse(fallbackDir),
+                        "image/",
+                        R.string.receiver_image_default_name),
+                // PDF
+                new Importer(this,
+                        taskExecutor,
+                        homeEnvironment.getDefaultDirectory(HomeEnvironment.DOCUMENTS)
+                                .orElse(fallbackDir),
+                        "application/pdf",
+                        R.string.receiver_pdf_default_name),
+                // Video
+                new Importer(this,
+                        taskExecutor,
+                        homeEnvironment.getDefaultDirectory(HomeEnvironment.MOVIES)
+                                .orElse(fallbackDir),
+                        "video/",
+                        R.string.receiver_video_default_name),
+        };
+    }
+
+    private void runImporter(Importer importer, Intent intent) {
+        importer.execute(intent.getParcelableExtra(Intent.EXTRA_STREAM),
+                fileName -> {
+                    final Dialog dialog = new AlertDialog.Builder(this, R.style.DialogTheme)
+                            .setMessage(getString(R.string.receiver_importing_message, fileName))
+                            .setCancelable(false)
+                            .create();
+                    dialogRef.getAndSet(Optional.of(dialog)).ifPresent(Dialog::dismiss);
+                    dialog.show();
+                },
+                (destination, fileName) -> {
+                    final Dialog dialog = new AlertDialog.Builder(this, R.style.DialogTheme)
+                            .setMessage(getString(R.string.receiver_importing_done_ok,
+                                    destination, fileName))
+                            .setPositiveButton(android.R.string.ok, (d, which) -> d.dismiss())
+                            .setOnDismissListener(d -> finish())
+                            .create();
+                    dialogRef.getAndSet(Optional.of(dialog)).ifPresent(Dialog::dismiss);
+                    dialog.show();
+                },
+                fileName -> {
+                    Log.e(TAG, "Failed to import " + fileName);
+                    final Dialog dialog = new AlertDialog.Builder(this, R.style.DialogTheme)
+                            .setMessage(getString(R.string.receiver_importing_done_fail, fileName))
+                            .setPositiveButton(android.R.string.ok, (d, which) -> d.dismiss())
+                            .setOnDismissListener(d -> finish())
+                            .create();
+                    dialogRef.getAndSet(Optional.of(dialog)).ifPresent(Dialog::dismiss);
+                    dialog.show();
+                }
+        );
     }
 }
