ab53bf57ed24c5c4d439458c5c56180548150663
2bllw8 <2bllw8@gmail.com>
2022-04-11 13:36:11 +0200

    Make EmptyCursor extend AbstractCursor rather than implementing Cursor

--- app/src/main/java/exe/bbllw8/anemo/documents/provider/EmptyCursor.java
+++ app/src/main/java/exe/bbllw8/anemo/documents/provider/EmptyCursor.java
@@ -4,89 +4,13 @@
  */
 package exe.bbllw8.anemo.documents.provider;
 
-import android.content.ContentResolver;
-import android.database.CharArrayBuffer;
-import android.database.ContentObserver;
-import android.database.Cursor;
-import android.database.DataSetObserver;
-import android.net.Uri;
-import android.os.Bundle;
+import android.database.AbstractCursor;
 
-public final class EmptyCursor implements Cursor {
+public final class EmptyCursor extends AbstractCursor {
 
     @Override
     public int getCount() {
         return 0;
-    }
-
-    @Override
-    public int getPosition() {
-        return 0;
-    }
-
-    @Override
-    public boolean move(int offset) {
-        return false;
-    }
-
-    @Override
-    public boolean moveToPosition(int position) {
-        return false;
-    }
-
-    @Override
-    public boolean moveToFirst() {
-        return false;
-    }
-
-    @Override
-    public boolean moveToLast() {
-        return false;
-    }
-
-    @Override
-    public boolean moveToNext() {
-        return false;
-    }
-
-    @Override
-    public boolean moveToPrevious() {
-        return false;
-    }
-
-    @Override
-    public boolean isFirst() {
-        return false;
-    }
-
-    @Override
-    public boolean isLast() {
-        return false;
-    }
-
-    @Override
-    public boolean isBeforeFirst() {
-        return false;
-    }
-
-    @Override
-    public boolean isAfterLast() {
-        return false;
-    }
-
-    @Override
-    public int getColumnIndex(String columnName) {
-        return 0;
-    }
-
-    @Override
-    public int getColumnIndexOrThrow(String columnName) throws IllegalArgumentException {
-        return 0;
-    }
-
-    @Override
-    public String getColumnName(int columnIndex) {
-        return null;
     }
 
     @Override
@@ -95,120 +19,37 @@ public final class EmptyCursor implements Cursor {
     }
 
     @Override
-    public int getColumnCount() {
-        return 0;
-    }
-
-    @Override
-    public byte[] getBlob(int columnIndex) {
-        return new byte[0];
-    }
-
-    @Override
-    public String getString(int columnIndex) {
+    public String getString(int column) {
         return null;
     }
 
     @Override
-    public void copyStringToBuffer(int columnIndex, CharArrayBuffer buffer) {
-
-    }
-
-    @Override
-    public short getShort(int columnIndex) {
+    public short getShort(int column) {
         return 0;
     }
 
     @Override
-    public int getInt(int columnIndex) {
+    public int getInt(int column) {
         return 0;
     }
 
     @Override
-    public long getLong(int columnIndex) {
-        return 0;
+    public long getLong(int column) {
+        return 0L;
     }
 
     @Override
-    public float getFloat(int columnIndex) {
-        return 0;
+    public float getFloat(int column) {
+        return 0f;
     }
 
     @Override
-    public double getDouble(int columnIndex) {
-        return 0;
+    public double getDouble(int column) {
+        return 0.0;
     }
 
     @Override
-    public int getType(int columnIndex) {
-        return 0;
-    }
-
-    @Override
-    public boolean isNull(int columnIndex) {
-        return false;
-    }
-
-    @Override
-    public void deactivate() {
-    }
-
-    @Override
-    public boolean requery() {
-        return false;
-    }
-
-    @Override
-    public void close() {
-
-    }
-
-    @Override
-    public boolean isClosed() {
-        return false;
-    }
-
-    @Override
-    public void registerContentObserver(ContentObserver observer) {
-    }
-
-    @Override
-    public void unregisterContentObserver(ContentObserver observer) {
-    }
-
-    @Override
-    public void registerDataSetObserver(DataSetObserver observer) {
-    }
-
-    @Override
-    public void unregisterDataSetObserver(DataSetObserver observer) {
-    }
-
-    @Override
-    public void setNotificationUri(ContentResolver cr, Uri uri) {
-    }
-
-    @Override
-    public Uri getNotificationUri() {
-        return null;
-    }
-
-    @Override
-    public boolean getWantsAllOnMoveCalls() {
-        return false;
-    }
-
-    @Override
-    public void setExtras(Bundle extras) {
-    }
-
-    @Override
-    public Bundle getExtras() {
-        return null;
-    }
-
-    @Override
-    public Bundle respond(Bundle extras) {
-        return null;
+    public boolean isNull(int column) {
+        return true;
     }
 }
